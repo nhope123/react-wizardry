8318a5fea6a36ec94fd81e569736370fda0933c0
Nial Hope <nyevincy@gmail.com>
2024-11-30 01:33:21 -0500

    Add 'Create Component' and 'Create React Hook' commands with associated icon

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import generateComponent from './commands/generateComponent/generateComponent';
+import generateHook from './commands/generateHook/generateHook';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -11,6 +12,9 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand('react-wizardry.generateComponent', generateComponent)
 	);
 
+	context.subscriptions.push(
+		vscode.commands.registerCommand('react-wizardry.createHook', generateHook)
+	);
 }
 
 // This method is called when your extension is deactivated
